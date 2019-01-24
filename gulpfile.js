7c7e68c23185729b6feb0a603ed2d77742c533d4
Ksenia Koldaeva <kseniacold@gmail.com>
2019-01-24 12:42:19 -0800

    Add gulp task for Less Watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { series } = require('gulp');
+const gulp = require('gulp');
 
 // The `clean` function is not exported so it can be considered a private task.
 // It can still be used within the `series()` composition.
@@ -7,14 +7,34 @@ const { series } = require('gulp');
 //   cb();
 // }
 
+
+const lessDirPath = 'src';
+const compiledDirPath = 'src';
+
 // The `build` function is exported so it is public and can be run with the `gulp` command.
 // It can also be used within the `series()` composition.
 function buildLess(cb) {
-  const lessDirPath = 'src';
-  const compiledDirPath = 'src';
-  require('less-recursive-compiler').compile(lessDirPath, compiledDirPath);
+  
+  require('less-recursive-compiler').compile(
+    lessDirPath, 
+    compiledDirPath,
+    {
+      // javascriptEnabled: true
+    }
+  );
   cb();
 }
 
+function watchLess(cb) {
+  gulp.watch(
+    [lessDirPath+'/*.less', lessDirPath+'/**/*.less'],
+    buildLess
+  )
+}
+
+gulp.task('buildLess', buildLess);
+gulp.task('watchLess', watchLess);
+
 exports.buildLess = buildLess;
+exports.watchLess = watchLess;
 //exports.default = series(buildLess);
