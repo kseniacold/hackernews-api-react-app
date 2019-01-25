d6480f12c1dd24f602dfc3c7334fa38e2ac8d151
Ksenia Koldaeva <kseniacold@gmail.com>
2019-01-25 09:01:57 -0800

    Start styles for HN page

--- src/HNHeader/HNHeader.js
+++ src/HNHeader/HNHeader.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './HNHeader.css';
+import heartIcon from './heart-nav.svg';
+
+
 const HNHeader = () => (
   <div className="HNHeader">
-    <Link style={{display: "inline-block", color: "black", marginRight: "50px"}} to="/hacker-news">All News</Link>
-    <Link style={{display: "inline-block", color: "black"}} to="/my-bookmarks">Bookmarks</Link>
+    <div className="HNHeader__logo">HN Reader</div>
+    <nav className="HNHeader__nav" delme="aaa">
+      <Link className="HNHeader__nav-link" to="/hacker-news">Top 100</Link>
+      <Link className="HNHeader__nav-link" to="/my-bookmarks">
+        <img src={heartIcon} alt="" /> My Bookmarks
+      </Link>
+    </nav>
+   
   </div>
 );
 
