820906c57b1d45c9d24a70383ac4c63553d6c5f0
Ksenia Koldaeva <kseniacold@gmail.com>
2019-01-24 21:02:39 -0800

    Add more routes. Set HashRouter

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import LandingPage from '../LandingPage/LandingPage';
 import HackerNews from '../HackerNews/HackerNews';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Bookmarks from '../Bookmarks/Bookmarks';
+import { HashRouter as Router, Route} from 'react-router-dom';
 
+import { createStore } from 'redux';
+import hackerNewsApp from '../reducers';
+
+// Create Redux Store of our application
+const store = createStore(hackerNewsApp);
 
 
 class App extends Component {
@@ -11,7 +17,14 @@ class App extends Component {
         <Router>
            <div className="App">
               <Route path="/" exact component={LandingPage} />
-              <Route path="/hacker-news" exact component={HackerNews} />
+              <Route 
+                path="/hacker-news" exact 
+                render={(props) => <HackerNews {...props} store={store} />}
+              />
+               <Route 
+                path="/my-bookmarks" exact 
+                render={(props) => <Bookmarks {...props} store={store} />}
+              />            
             </div>   
         </Router>
     );
