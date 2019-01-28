ea9fabb9c5f9c492c3f57f2ea52a16d3b8ddfa6e
Ksenia Koldaeva <kseniacold@gmail.com>
2019-01-27 23:01:04 -0800

    HackerNews component - formatting

--- src/HackerNews/HackerNews.js
+++ src/HackerNews/HackerNews.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import HNHeader from '../HNHeader/HNHeader';
 import StoriesContainer from '../StoriesContainer/StoriesContainer';
 
-
 const HackerNews = ({store}) => (
   <div className="HackerNews">
     <HNHeader top100isSelected={true} />
