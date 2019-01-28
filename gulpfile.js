ecf1a5abe551b5d26c66c89c2bbebf7bcd2b0390
Ksenia Koldaeva <kseniacold@gmail.com>
2019-01-27 17:35:34 -0800

    Create buildLess task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,22 @@
-const { spawn } = require('child_process');
 const gulp = require('gulp');
-const less = require('less');
-
-// The `clean` function is not exported so it can be considered a private task.
-// It can still be used within the `series()` composition.
-// function clean(cb) {
-//   // body omitted
-//   cb();
-// }
-
+const less = require('gulp-less');
 
 const lessDirPath = 'src';
 const compiledDirPath = 'src';
 
-// The `build` function is exported so it is public and can be run with the `gulp` command.
-// It can also be used within the `series()` composition.
-function buildLess(cb) {
-  
-  require('less-recursive-compiler').compile(
-    lessDirPath, 
-    compiledDirPath,
-    {
-      // javascriptEnabled: true
-    }
-  );
-  cb();
+/**
+ * Compiles .less files recursively 
+ * And puts output .css files in the initial foldes
+ */
+function buildLess() {
+  return gulp.src('./' + lessDirPath + '/**/*.less')
+  .pipe(less({
+    javascriptEnabled: true
+  }))
+  .pipe(gulp.dest('./' + compiledDirPath));
 }
 
-// TODO clean this up
-function watchLess(cb) {
-  gulp.watch(
-    [lessDirPath+'/**/*.less'],
-    function(cb){cb()}
-  )
-  .on('change', function(path, stats){
-    //console.log('File ' + path + ' was changed');
-    let outPath = path.replace('.less', '.css');
-    let cmd = `/usr/bin/lessc ${path} ${outPath}`;
-    console.log(`Running ${cmd}`);
-    // spawn(cmd);
-  })
-}
-
+// Create gulp task to use in package.json
 gulp.task('buildLess', buildLess);
-gulp.task('watchLess', watchLess);
 
 exports.buildLess = buildLess;
-exports.watchLess = watchLess;
-//exports.default = series(buildLess);
