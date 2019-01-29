b2e1db973b8333bf1a72ed78bc25710e7b9be10e
Ksenia Koldaeva <kseniacold@gmail.com>
2019-01-28 19:13:49 -0800

    Refactor Navigation in a separate component

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,11 +3,13 @@ import MediaQuery from 'react-responsive';
 import HeaderDesktop from '../HeaderDesktop/HeaderDesktop';
 import HeaderMobile from '../HeaderMobile/HeaderMobile';
 
+import menus from '../data/menus';
+
 const Header = ({toggleMenu}) => (
     <MediaQuery minWidth={1200}>
         {matches => {
           return matches
-            ? <HeaderDesktop />
+            ? <HeaderDesktop menus={menus}/>
             : <HeaderMobile toggleMenu={toggleMenu}/>
         }}
       </MediaQuery>
--- src/HeaderDesktop/HeaderDesktop.js
+++ src/HeaderDesktop/HeaderDesktop.js
@@ -1,38 +1,15 @@
 import React from 'react';
+import Navigation from '../Navigation/Navigation';
 import logo from './../logo.svg';
 
 import './HeaderDesktop.css';
 
-const HeaderDesktop = () => (
+const HeaderDesktop = ({menus}) => (
     <header className="HeaderDesktop">
         <div className="HeaderDesktop__logo">
             <img src={logo} className="HeaderDesktop__logo-img" alt="Sure App Logo" />
         </div>
-        <nav className="HeaderDesktop__nav nav">
-            <ul className="HeaderDesktop__nav-right">
-                <li className="nav__item nav__item_dropdown">
-                    <a href="/" className="nav__link">Products</a>
-                    <i className="nav__dropdown-icon material-icons">arrow_drop_down</i>
-                </li>
-                <li className="nav__item">
-                    <a href="/" className="nav__link">Support</a>
-                </li>
-                <li className="nav__item">
-                    <a href="/" className="nav__link">Claims</a>
-                </li>
-            </ul>
-            <ul className="HeaderDesktop__nav-left">
-                <li className="nav__item">
-                    <a href="/" className="nav__link">Download App</a>
-                </li>
-                <li className="nav__item">
-                    <a href="/" className="nav__link">Login</a>
-                </li>
-                <li className="nav__item nav__item_button-style">
-                    <a href="/" className="nav__link">Sign Up</a>
-                </li>
-            </ul>
-        </nav>
+        <Navigation menus={menus} isDesktop={true}/>
     </header>
 );
 
